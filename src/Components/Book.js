1aa14e94739ec2f3b9644a84739a101210e081ed
Youssef Girgeis <youssefgirgeis@gmail.com>
2022-01-25 23:35:39 -0500

    fetch and display all books

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,6 +1,6 @@
 import BookShelfChanger from './BookShelfChanger';
 
-function Book() {
+function Book({ bookTitle, authors, imageLink }) {
   return (
     <div className='book'>
       <div className='book-top'>
@@ -9,14 +9,13 @@ function Book() {
           style={{
             width: 128,
             height: 193,
-            backgroundImage:
-              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+            backgroundImage: `url('${imageLink}')`,
           }}
         />
         <BookShelfChanger />
       </div>
-      <div className='book-title'>To Kill a Mockingbird</div>
-      <div className='book-authors'>Harper Lee</div>
+      <div className='book-title'>{bookTitle}</div>
+      <div className='book-authors'>{authors}</div>
     </div>
   );
 }
